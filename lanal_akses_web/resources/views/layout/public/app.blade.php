9019c7b6c709a715a18a8d77923f8b918d744ce4
Dava Adriansyah <davahanyanama@gmail.com>
2023-09-04 10:39:14 +0700

    menambahkan layouting footer untuk public

--- lanal_akses_web/resources/views/layout/public/app.blade.php
+++ lanal_akses_web/resources/views/layout/public/app.blade.php
@@ -16,6 +16,8 @@
   <body>
     
     @yield('content')
+
+    @extends('layout.public.footer')
     <!-- Optional JavaScript -->
     {{-- Icon --}}
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
