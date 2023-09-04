f47cac4dc21c9f14c09e5c3739282ae33df9e211
Dava Adriansyah <davahanyanama@gmail.com>
2023-09-04 10:34:20 +0700

    menambahkan halaman admin dashboard

---
+++ lanal_akses_web/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.admin.app')
+
+@section('title-page', 'Admin | Dashboard')
+
+@section('content')
+    <h1 class="text-black">Dashboard</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi sint, temporibus consectetur error voluptatibus officiis totam quibusdam sunt facilis non blanditiis doloremque, quia natus exercitationem, quas tenetur! Itaque quidem ex impedit consectetur laborum accusamus quaerat ipsam deserunt! Alias recusandae blanditiis adipisci asperiores similique ut tenetur sapiente? Impedit ullam quasi provident!</p>
+@endsection
