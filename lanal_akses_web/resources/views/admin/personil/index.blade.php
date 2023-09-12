6824e8676a7e7c99ce0a15ee7db812ed0042037d
Dava Adriansyah <davahanyanama@gmail.com>
2023-09-12 09:53:37 +0700

    commit pertama halaman personil

---
+++ lanal_akses_web/resources/views/admin/personil/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.admin.app')
+
+@section('title-page', 'Admin | Data Personil')
+
+@section('content')
+    <h1>Data Personil</h1>
+@endsection
--- lanal_akses_web/resources/views/layout/admin/sidebar.blade.php
+++ lanal_akses_web/resources/views/layout/admin/sidebar.blade.php
@@ -1,29 +1,29 @@
 <div id="sidebar-collapse" class="sidebar fixed-top top-0 bottom-0 bg-greenmain  p-4 " style="height: 100%">
-    <div id="sidebar-icon" class="d-flex justify-content-end">
+    <div id="sidebar-icon" class="d-flex justify-content-end mb-4">
         <iconify-icon class="text-white" id="icon-menu" icon="material-symbols:menu" width="36"></iconify-icon>
         <iconify-icon class="text-white" id="icon-close" icon="material-symbols:close" width="36"></iconify-icon>
     </div>
-    <a href="{{ url('/') }}" id="sidebar-brand" class="text-large text-white text-uppercase my-3"><img src="https://i.ibb.co/MR438ww/logo-no-bg.png" alt="logo-no-bg" border="0" width="30rem" height="auto" class="mr-2"><span id="sidebar-title">Lanal Akses</span> </a>
+    <a href="{{ url('/') }}" id="sidebar-brand" class="text-large text-white text-uppercase mx-3"><img src="https://i.ibb.co/MR438ww/logo-no-bg.png" alt="logo-no-bg" border="0" width="30rem" height="auto" class="mr-2"><span id="sidebar-title">Lanal Akses</span> </a>
 
     <ul id="sidebar-menu" class="my-5 p-0">
         <a class="p-3 sidebar-item text-uppercase text-white rounded {{ (request()->is('admin')) ? 'active-sidebar-item' : '' }}" href="{{  url('#') }}">
-            <iconify-icon class="mr-3" icon="ic:{{ (request()->is('admin')) ? 'home' : 'outline-home' }}" href="{{  url('#') }}home" width="24"></iconify-icon>
+            <iconify-icon class="mr-3" icon="ic:{{ (request()->is('admin')) ? 'home' : 'outline-home' }}" href="{{  url('#') }}" width="24"></iconify-icon>
             <span class="sidebar-text">Dashboard</span>
         </a>
         <a class="p-3 sidebar-item text-uppercase text-white rounded {{ (request()->is('admin/pns')) ? 'active-sidebar-item' : '' }}" href="{{  url('#') }}" href="{{  url('#') }}">
-            <iconify-icon class="mr-3" icon="material-symbols:group" width="24"></iconify-icon>
+            <iconify-icon class="mr-3" icon="material-symbols:{{ (request()->is('admin/pns')) ? 'group' : 'group-outline' }}" width="24"></iconify-icon>
             <span class="sidebar-text">Data PNS</span>   
         </a>
         <a class="p-3 sidebar-item text-uppercase text-white rounded {{ (request()->is('admin/personil')) ? 'active-sidebar-item' : '' }}" href="{{  url('#') }}" href="{{  url('#') }}">
-            <iconify-icon class="mr-3" icon="material-symbols:group" width="24"></iconify-icon>
+            <iconify-icon class="mr-3" icon="material-symbols:{{ (request()->is('admin/personil')) ? 'group' : 'group-outline' }}" width="24"></iconify-icon>
             <span class="sidebar-text">Data Personil</span>
         </a>
         <a class="p-3 sidebar-item text-uppercase text-white rounded {{ (request()->is('admin/absensi')) ? 'active-sidebar-item' : '' }}" href="{{  url('#') }}" href="{{  url('#') }}">
-            <iconify-icon class="mr-3" icon="mdi:clock" width="24"></iconify-icon>
+            <iconify-icon class="mr-3" icon="mdi:{{ (request()->is('admin/absensi')) ? 'clock' : 'clock-outline' }}" width="24"></iconify-icon>
             <span class="sidebar-text">Data Absensi</span>   
         </a>
         <a class="p-3 sidebar-item text-uppercase text-white rounded {{ (request()->is('admin/tambah')) ? 'active-sidebar-item' : '' }}" href="{{  url('#') }}" href="{{  url('#') }}">
-            <iconify-icon class="mr-3" icon="ic:baseline-person-add" width="24"></iconify-icon>
+            <iconify-icon class="mr-3" icon="ic:{{ (request()->is('admin/tambah')) ? 'baseline-person-add' : 'baseline-person-add-alt' }}" width="24"></iconify-icon>
             <span class="sidebar-text">Tambah User</span>    
         </a>
         
@@ -44,7 +44,6 @@
         $(".content-margin").removeClass("content-wrap");
         $("#sidebar-icon").removeClass("justify-content-end");
         $("#sidebar-icon").addClass("justify-content-center");
-        $("#sidebar-brand").addClass("text-center");
         $("#logout-button").text("");
         $("#logout-button").html('<iconify-icon icon="ion:exit-outline" width="24"></iconify-icon>');
         $("#sidebar-collapse").removeClass("p-4");
@@ -64,7 +63,6 @@
         $(".content-margin").addClass("content-wrap");
         $("#sidebar-icon").addClass("justify-content-end");
         $("#sidebar-icon").removeClass("justify-content-center");
-        $("#sidebar-brand").removeClass("text-center");
         $("#logout-button").html('Logout <iconify-icon class="ml-2" icon="ion:exit-outline" width="24"></iconify-icon>');
         $(".sidebar-item").css({
             "margin": "1rem 1rem 1rem 0",
